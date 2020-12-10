6b9c12bdce696b5715941a0104e83b8ffffb7356
Nicholas Rossi <nrrossi@iu.edu>
2020-12-09 23:06:28 -0500

    Camera tracking
    Camer tracking is working using if statements and max and min values. Going to test something with detecting falling.

---
+++ Cheese Louise/Assets/CameraFollow.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+
+    public Transform target;
+
+
+
+    float xMax = 4f;
+    float xMin = -4f;
+    float yMax = 2.83f;
+    float yMin = -2.83f;
+
+    
+
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        //prevent camera from boundary. Shouldn't have to worry about the top.
+
+        //Bottom Left Case
+        if(target.position.x < xMin && target.position.y < yMin){
+            transform.position = new Vector3(xMin, yMin, -10);
+        }
+        //Bottom Right Case
+       else if(target.position.x > xMax && target.position.y < yMin){
+            transform.position = new Vector3(xMax, yMin, -10);
+        }
+        //Bottom Case
+        else if(target.position.y < yMin){
+            transform.position = new Vector3(target.position.x, yMin, -10);
+        }
+        //Left Case{
+        else if(target.position.x < xMin){
+            transform.position = new Vector3(xMin, target.position.y, -10);
+        }
+        //Right Case
+        else if(target.position.x > xMax){
+            transform.position = new Vector3(xMax, target.position.y, -10);
+        }
+        else{
+            //Camera Follows Player
+            transform.position = new Vector3(target.position.x, target.position.y, -10);
+        }
+    }
+}
