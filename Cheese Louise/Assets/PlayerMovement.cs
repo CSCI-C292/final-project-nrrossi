6c2c7faad75dbaacc1cea79b55a64ebcefd37ded
Nicholas Rossi <nrrossi@iu.edu>
2020-12-09 21:37:51 -0500

    Movement complete again.
    Made it so the player can only walk up pipes. there is still the problem of movement when falling but i want to work on the camera first.

--- Cheese Louise/Assets/PlayerMovement.cs
+++ Cheese Louise/Assets/PlayerMovement.cs
@@ -11,11 +11,13 @@ public class PlayerMovement : MonoBehaviour
     public Animator animator;
 
     Vector2 movement;
+
     void Update() 
     {
+        //Gets movements inputs in x-axis
         movement.x = Input.GetAxisRaw("Horizontal");
-        movement.y = Input.GetAxisRaw("Vertical");
 
+        //Sets animator stuff...
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
@@ -23,10 +25,24 @@ public class PlayerMovement : MonoBehaviour
 
 
     void FixedUpdate() 
-    {
+    {   //Moves the character
         rb.MovePosition(rb.position + movement * _moveSpeed * Time.fixedDeltaTime);
     }
 
 
+    //When player in on a pipe, allows them to climb it
+    private void OnTriggerStay2D(Collider2D other) 
+    {   
+        if(other.CompareTag("Pipe"))
+            movement.y = Input.GetAxisRaw("Vertical");
+
+        Debug.Log(other.tag);
+    }
+
+    //when player is off a pipe, prevents them from climbing.
+    private void OnTriggerExit2D(Collider2D other) 
+    {
+        movement.y = 0;
+    }
 
 }
