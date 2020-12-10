193bf4a7ccf738ff6fba390c310d63b27d61473b
Nicholas Rossi <nrrossi@iu.edu>
2020-12-09 20:44:23 -0500

    Movement Complete
    Momement and animation complete. Need to tweak movement so you can only move up pipes.

--- Cheese Louise/Assets/PlayerMovement.cs
+++ Cheese Louise/Assets/PlayerMovement.cs
@@ -8,10 +8,25 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] float _moveSpeed = 5f;
 
     public Rigidbody2D rb;
+    public Animator animator;
 
-    // Update is called once per frame
-    void Update()
+    Vector2 movement;
+    void Update() 
     {
-        
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        animator.SetFloat("Horizontal", movement.x);
+        animator.SetFloat("Vertical", movement.y);
+        animator.SetFloat("Speed", movement.sqrMagnitude);
     }
+
+
+    void FixedUpdate() 
+    {
+        rb.MovePosition(rb.position + movement * _moveSpeed * Time.fixedDeltaTime);
+    }
+
+
+
 }
