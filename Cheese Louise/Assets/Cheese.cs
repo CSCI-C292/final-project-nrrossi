95032aad0e79db2c90c1367d64122194e6187dfb
Nicholas Rossi <nrrossi@iu.edu>
2020-12-13 15:45:34 -0500

    Cheese and Icon Complete
    When mouse runs over cheese, cheese on level dissapears, Cheese icon appears, and hsCheese is set to true for the MouseHole

---
+++ Cheese Louise/Assets/Cheese.cs
@@ -0,0 +1,39 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Cheese : MonoBehaviour
+{
+
+    private bool playerHasCheese = false;
+    public GameObject cheeseForMouse; 
+    public Image cheeseIcon;
+
+    private void Awake() {
+        cheeseIcon.gameObject.SetActive(false); //creates the Icon and make it inactive
+    }
+
+    //Get boolean for Mousehole.
+    public bool hasCheese(){
+        return playerHasCheese;
+    }
+
+
+    //Function that sets cheese bool to true and activates cheese icon and diactivates cheese on level
+    public void playerGotCheese(){
+        playerHasCheese = true;     //Sets Boolean to true
+        cheeseIcon.gameObject.SetActive(true);  //Enables Cheese Icon
+        cheeseForMouse.SetActive(false);    //Disables Cheese On Map
+    }
+
+    //Checks if mouse touched cheese and calls playerGotCheese Function
+    private void OnTriggerEnter2D(Collider2D other) {
+        if(other.CompareTag("Cheese")){
+            playerGotCheese();
+        }
+    }
+
+
+}
